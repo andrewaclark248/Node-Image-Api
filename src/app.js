c2f00f2565572f6ee6bb5b3848195e088614c782
andrewaclark248 <andrewaclark248@gmail.com>
2023-05-27 12:44:50 -0400

    add routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import express from 'express'
+import routes from './routes/index.js'
 
 const app = express();
 const port = 3000;
 
-//define a route handler for the default home page
-app.get("/api", (req, res) => {
-    res.send("sessrver working")
-})
+app.use("/api", routes)
 
 app.listen(port, () => {
     console.log(" serverted started on port 3000")
