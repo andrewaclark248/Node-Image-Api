2565b1dc32fe7f5a992eb1b1834f3ad0ab93c2f1
andrewaclark248 <andrewaclark248@gmail.com>
2023-05-28 15:32:25 -0400

    write to file

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,11 +1,46 @@
+import sharp from 'sharp'
+import fs from 'fs';
+import * as path from 'path';
 
 class AuthController {
 
-    show(req, res, next) {
+    async show(req, res, next) {
+        let fileName = req.query?.filename
 
-        //res.sendFile(fileName)
-        console.log("api request created haahahh");
-        res.send("show action response");
+        if (fileName != null) {
+            let file = "./../assets/full/fjord.jpg"
+
+            let file2 = "./src/assets/full/fjord.jpg"
+
+            var data = fs.readFileSync(file2);
+
+            let newImage = await sharp(data)
+                                    .resize({
+                                        width: 150,
+                                        height: 150
+                                    }).toBuffer()
+                                    //.toFile("./output.png"); //`${fileName}.png`
+            
+            const fileContent = "This is an example";
+ 
+            let filePath = "./src/assets/thumb/output.png"
+            fs.writeFileSync(filePath, newImage, { flag: 'w' }, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully Written to File.");
+
+                }
+            });
+            
+            //res.sendFile(path [, options] [, fn])
+            res.send("send response");
+
+
+        } else {
+            res.send("please provide a filename; not provided");
+        }
+
     }
 
 }
