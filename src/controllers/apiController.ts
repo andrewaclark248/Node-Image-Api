12b288d961c3212d87e26f1164245d163ffaa7b7
andrewaclark248 <andrewaclark248@gmail.com>
2023-05-29 21:31:09 -0400

    add utils file

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -3,48 +3,7 @@ import fs from 'fs';
 import * as path from 'path';
 import { Request, Response, NextFunction, Router } from 'express';
 import { GetFileInterface, FileName } from './../interfaces/apiInterface';
-
-function getFile(fileName: FileName, dimensionPresent: boolean, parsedFileName: FileName): GetFileInterface {
-  if (!dimensionPresent) {
-      return { dimensionsPresent: false, fileExist: false, create: false, fileFullPath: "", fileThumbPath: "" };
-  }
-
-  //set initial GetFileInterface paramas
-  let create: boolean = false
-  let fileFullPath: string = '';
-  let fileThumbPath: string = '';
-
-  //file paths
-  let pathThumbJpg = `./src/assets/thumb/${fileName.parsedName}.jpg`;
-  let pathThumbPng = `./src/assets/thumb/${fileName.parsedName}.png`;
-  let pathFullJpg = `./src/assets/full/${fileName.file}.jpg`;
-  let pathFullPng = `./src/assets/full/${fileName.file}.png`;
-
-  try {
-    //check if file exists in full path
-    if (fs.existsSync(pathFullJpg) || fs.existsSync(pathFullPng)) { 
-        fileFullPath = fs.existsSync(pathFullJpg) ? pathFullJpg : pathFullPng
-        fileThumbPath = fs.existsSync(pathFullJpg) ? pathThumbJpg : pathThumbPng
-
-        if (fs.existsSync(pathThumbJpg) ) {
-            create = false;
-        } else if (fs.existsSync(pathThumbPng)) {
-            create = false;
-        } else {
-            create = true;
-        }
-
-        return { dimensionsPresent: true, fileExist: true, create: create, fileFullPath: fileFullPath, fileThumbPath: fileThumbPath }
-
-    } else {
-        return { dimensionsPresent: true, fileExist: false, create: false, fileFullPath: "", fileThumbPath: ""};
-    }
-
-  } catch (err) {
-    console.error(err);
-  }
-  return { dimensionsPresent: false, fileExist: false, create: false, fileFullPath: "", fileThumbPath: "" }
-}
+import { getFile } from './../utils/index'
 
 
 export async function show(req: Request, res: Response, next: NextFunction) {
