4eccb5e2f5bd53f3f1d5e5d3bd57db8c40ecf799
andrewaclark248 <andrewaclark248@gmail.com>
2023-06-26 20:10:27 -0400

    specify type in parameter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 const routes = express.Router();
+import { Request, Response } from 'express';
 
 //define a route handler for the default home page
-routes.get('/', (req, res): void => {
+routes.get('/', (req: Request, res: Response): void => {
   res.send('main page');
 });
 
